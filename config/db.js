bb2550c6964003a6d92eeddb39d896037d1f9388
hamdi <charradihamdi1@gmail.com>
2023-01-12 22:59:54 +0100

    deploy server

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-//mongodb+srv://cluster0.rfqp4.mongodb.net/seomarket
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.8pl1w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.lm87vwy.mongodb.net/?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
